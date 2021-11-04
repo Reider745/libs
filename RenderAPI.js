672d341e1330527c9f68659d439a2202b603ad12
Reider745 <70357814+Reider745@users.noreply.github.com>
2021-11-04 08:53:40 +0300

    Update
    -add RenderAPI.Animation
    -added handler animation

--- RenderAPI.js
+++ RenderAPI.js
@@ -9,7 +9,7 @@
 */
 LIBRARY({
 	name: "RenderAPI",
-	version: 1,
+	version: 2,
 	shared: true,
 	api: "CoreEngine"
 });
@@ -27,12 +27,14 @@ let RenderAPI = {
 				id: id || 1,
 				data: data || 0
 			};
+			return this;
 		}
 		this.getBoxes = function(){
 			return boxes;
 		}
 		this.setBoxes = function(arr){
 			boxes = arr;
+			return this;
 		}
 		this.getAllName = function(){
 			return Object.keys(boxes);
@@ -61,6 +63,7 @@ let RenderAPI = {
 			data = data || -1
 			BlockRenderer.setStaticICRender(id, data, this.getICRenderModel());
 			BlockRenderer.setCustomCollisionAndRaycastShape(id, data, this.getCollisionShape())
+			return this; 
 		}
 		this.copy = function(){
 			let model = new RenderAPI.Model();
@@ -74,6 +77,11 @@ let RenderAPI = {
 		let start = new RenderAPI.Model();
 		let end = new RenderAPI.Model();
 		let update = {};
+		let handler = {
+			start(){},
+			update(frame){},
+			end(){}
+		};
 		
 		this.setTime = function(tick){
 			time = tick;
@@ -131,16 +139,20 @@ let RenderAPI = {
 		}
 		this.play = function(x, y, z, infinite){
 			let _this = this;
+			handler.start();
 			Updatable.addUpdatable({
 				tick: 0,
 				update(){
 					BlockRenderer.mapAtCoords(x, y, z, _this.getModelByFrame(this.tick).getICRenderModel());
+					handler.update(tick);
 					this.tick++;
 					if(this.tick >= time){
 						if(infinite){
 							this.tick = 0;
-							_this.replaceModel()
+							_this.replaceModel();
+							handler.end();
 						}else{
+							handler.end();
 							this.remove = true
 						}
 					}
@@ -148,14 +160,150 @@ let RenderAPI = {
 			})
 		}
 		this.updateModel = function(x, y, z, infinite){
+			if(frame == 0)
+				handler.start();
 			BlockRenderer.mapAtCoords(x, y, z, this.getModelByFrame(frame).getICRenderModel());
+			handler.update(frame);
 			frame++;
 			if(frame >= time){
 				frame = 0;
+				handler.end();
 				if(infinite)
 					this.replaceModel();
 			}
 		}
+		this.setHandler = function(obj){
+			handler = {
+				start: obj.start || function(){},
+				update: update.end || function(frame){},
+				end: obj.end || function(){},
+			}
+		}
+	},
+	Animation(model, obj){
+		let keys = Object.keys(obj);
+		for(let i in keys)
+			keys[i] = parseInt(keys[i]);
+		let arr = keys.sort(function(a, b){
+			return a - b;
+		});
+		
+		let time = 80;
+		let tick = 0;
+		let handler = {
+			start(){},
+			update(frame){},
+			end(){}
+		};
+		
+		this.setTime = function(time_anim){
+			time = time_anim;
+			tick = 0;
+		}
+		this.getTime = function(){
+			return {
+				time: time,
+				frame: frame
+			}
+		}
+		this.setAnimation = function(model2, obj){
+			model = model2;
+			let keys = Object.keys(obj);
+			for(let i in keys)
+				keys[i] = parseInt(keys[i]);
+			arr = keys.sort(function(a, b){
+				return a - b;
+			});
+		}
+		this.getModels = function(tick){
+			let t_p = 0;
+			for(let i in arr){
+				let t = arr[i];
+				if(tick < t && tick >= t_p)
+					return {
+						pre: t_p == 0 ? model : obj[t_p],
+						post: obj[t] || obj[t_p],
+						time: t - t_p
+					};
+				else
+					t_p = t;
+			}
+			return {
+				pre: obj[arr[arr.length-1]],
+				post: obj[arr[arr.length-1]],
+				time: 1
+			};
+		}
+		let animation = new RenderAPI.ModelAnimation();
+		this.updateModel = function(x, y, z){
+			if(tick == 0){
+				let models = this.getModels(0);
+				animation.setModel(model, models.post);
+				animation.setTime(models.time);
+				handler.start();
+				let _this = this;
+				animation.setHandler({
+					end(){
+						let models = _this.getModels(tick+1);
+						animation.setModel(models.pre, models.post);
+						animation.setTime(models.time);
+					}
+				})
+			}
+			animation.updateModel(x, y, z, false);
+			handler.update(tick);
+			tick++;
+			let t = animation.getTime();
+			if(tick >= time){
+				handler.end();
+				tick = 0
+			}
+		}
+		this.play = function(x, y, z){
+			let animation = new RenderAPI.ModelAnimation();
+			let models = this.getModels(0);
+			animation.setModel(model, models.post);
+			animation.setTime(models.time);
+			handler.start();
+			let frame = 0;
+			let _this = this;
+			animation.setHandler({
+				end(){
+					let models = _this.getModels(frame+1);
+					animation.setModel(models.pre, models.post);
+					animation.setTime(models.time);
+				}
+			});
+			Updatable.addUpdatable({
+				update(){
+					animation.updateModel(x, y, z, false);
+					frame++;
+					handler.update(frame);
+					if(frame >= time){
+						handler.end();
+						this.remove = true;
+					}
+				}
+			});
+		}
+		this.setHandler = function(obj){
+			handler = {
+				start: obj.start || function(){},
+				update: update.end || function(frame){},
+				end: obj.end || function(){},
+			}
+		}
 	}
 };
+/*BlockRenderer.enableCoordMapping(98, -1, new RenderAPI.Model().addBoxByBlock(null, 0, 0, 0, .5, .5, .5).getICRenderModel())
+let animation = new RenderAPI.Animation(new RenderAPI.Model().addBoxByBlock(null, 0, 0, 0, .5, .5, .5), {
+	"25": new RenderAPI.Model().addBoxByBlock(null, 0, 0, .5, .5, .5, 1),
+	"50": new RenderAPI.Model().addBoxByBlock(null, .5, 0, .5, 1, .5, 1),
+	"75": new RenderAPI.Model().addBoxByBlock(null, .5, 0, 0, 1, .5, .5),
+	"100": new RenderAPI.Model().addBoxByBlock(null, 0, 0, 0, .5, .5, .5)
+});
+animation.setTime(100);
+Callback.addCallback("ItemUse", function(coords){
+	animation.play(coords.x,coords.y,coords.z);
+});*/
 EXPORT("RenderAPI", RenderAPI);
