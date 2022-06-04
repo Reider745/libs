2f81ad228c7a781fdda345d66f4418a2fc3cc012
Reider745 <fhgfdf574@gmail.com>
2022-06-04 15:07:07 +0300

    - add RenderUtil declaration

---
+++ models/RenderUtil.d.ts
@@ -0,0 +1,48 @@
+/// <reference path="./core-engine.d.ts"/>
+
+declare namespace RenderUtil {
+    class Model {
+        addBoxByBlock(name:string, x1:number, y1:number, z1:number, x2:number, y2:number, z2:number, id:number, data:number): Model;
+        getBoxes(): any;
+        setBoxes(boxes: any);
+        getAllName(): string[];
+        getBlockRender(): BlockRenderer.Model;
+        getCollisionShape(): ICRender.CollisionShape;
+        getICRenderModel(): ICRender.Model;
+        setBlockModel(id:number,data:number): Model;
+        copy(): Model;
+        getRenderMesh(): RenderMesh;
+    }
+
+    interface IHandlerAnimation {
+        start(): void;
+        update(frame: number): void;
+        end(): void;
+    }
+
+    class ModelAnimation {
+        setTime(tick: number): void;
+        getTime(): number;
+        setModel(start: Model, end: Model): void;
+        replaceModel(): void;
+        getTransferFrame(): any;
+        getModelByFrame(frame: number): Model;
+        play(x: number, y: number, z: number, infinite: boolean): void;
+        updateModel(x: number, y: number, z: number, infinite: boolean): void;
+        setHandler(handler: IHandlerAnimation): void;
+    }
+
+    class Animation {
+        constructor(model: Model, obj: {[key: string]: Model});
+        setTime(tick: number): void;
+        getTime(): number;
+        setAnimation(model: Model, obj: {[key: string]: Model}): void;
+        getModels(tick:number):any;
+        updateModel(x:number,y:number,z:number):void;
+        play(x:number,y:number,z:number):void;
+        setHandler(handler: IHandlerAnimation): void;
+    }
+
+    function convertModel(model: any): any;
+    function meshCopy(original: RenderMesh, mesh?:RenderMesh): RenderMesh;
+}
